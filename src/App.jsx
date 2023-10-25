60b49fe1d5b0e0ee2fba522889bf5418dccef2a6
VRXtucci <tucci.jose023@gmail.com>
2023-10-25 15:11:50 -0400

    agregando camponente de ejemplo con tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom'
+import Tailwind from './features/shared/pages/Prueva/Tailwind.jsx'
 import { BaseLayout } from './features/shared/pages/layouts/base-layout'
 import { HomePage } from './features/shared/pages/HomePage/home-page'
 import { LoginPage } from './features/auth/pages/LoginPage/login-page'
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <Router>
+      <Tailwind/>
       <Routes>
             <Route
               path="/"
