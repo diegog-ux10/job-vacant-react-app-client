819099dc179a1317e91f8b88d7ff82308d638f8a
DaveFont19 <davidfontes20@gmail.com>
2023-10-26 14:59:23 -0600

    cards en homepage

--- src/features/shared/pages/HomePage/home-page.jsx
+++ src/features/shared/pages/HomePage/home-page.jsx
@@ -1,27 +1,16 @@
 /* eslint-disable no-empty-pattern */
 // import PropTypes from 'prop-types';
+import Card from '../../../vacant/components/card'
 
 export const HomePage = () => {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-      <div className="bg-white p-4 shadow-md">
-        <p className="text-xl font-bold">Vacante 1</p>
-      </div>
-      <div className="bg-white p-4 shadow-md">
-        <p className="text-xl font-bold">Vacante 2</p>
-      </div>
-      <div className="bg-white p-4 shadow-md">
-        <p className="text-xl font-bold">Vacante 3</p>
-      </div>
-      <div className="bg-white p-4 shadow-md">
-        <p className="text-xl font-bold">Vacante 4</p>
-      </div>
-      <div className="bg-white p-4 shadow-md">
-        <p className="text-xl font-bold">Vacante 5</p>
-      </div>
-      <div className="bg-white p-4 shadow-md">
-        <p className="text-xl font-bold">Vacante 6</p>
-      </div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </section>
   )
 }
