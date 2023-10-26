73416141950b6acc26d8b6e6f93dd9ff3c4e36a1
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-26 12:17:36 -0400

    refactor: base layout and auth layout

--- src/features/shared/pages/HomePage/home-page.jsx
+++ src/features/shared/pages/HomePage/home-page.jsx
@@ -2,7 +2,24 @@
 // import PropTypes from 'prop-types';
 
 export const HomePage = () => {
-	return <div>
-		<h3 className="text-white">Hola cuate</h3>
-	</div>;
+	return       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+	<div className="bg-white p-4 shadow-md">
+	  <p className="text-xl font-bold">Vacante 1</p>
+	</div>
+	<div className="bg-white p-4 shadow-md">
+	  <p className="text-xl font-bold">Vacante 2</p>
+	</div>
+	<div className="bg-white p-4 shadow-md">
+	  <p className="text-xl font-bold">Vacante 3</p>
+	</div>
+	<div className="bg-white p-4 shadow-md">
+	  <p className="text-xl font-bold">Vacante 4</p>
+	</div>
+	<div className="bg-white p-4 shadow-md">
+	  <p className="text-xl font-bold">Vacante 5</p>
+	</div>
+	<div className="bg-white p-4 shadow-md">
+	  <p className="text-xl font-bold">Vacante 6</p>
+	</div>
+  </section>
 };
