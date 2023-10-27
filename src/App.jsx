fdf81a244ad6ea92e8b344601458ff8d7f1e2a35
VRXtucci <tucci.jose023@gmail.com>
2023-10-27 16:26:49 -0400

    agregando ruta del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom'
+import { JobInfo } from './features/job-vacancy/components/JobInfo'
 import { BaseLayout } from './features/shared/layouts'
 import { AuthLayout } from './features/auth/layouts'
 import { AdminPage, HomePage } from './features/shared/pages'
@@ -13,28 +14,35 @@ function App() {
   return (
     <Router>
       <Routes>
-            <Route
-              path="/"
-              element={
-                <BaseLayout>
-                  <HomePage />
-                </BaseLayout>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <AuthLayout>
-                  <LoginPage />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/admin"
-              element={
-                <AdminPage />
-              }
-            />
+        <Route
+          path="/"
+          element={
+            <BaseLayout>
+              <HomePage />
+            </BaseLayout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <AuthLayout>
+              <LoginPage />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <AdminPage />
+          }
+        />
+
+        <Route
+          path="/jobinfo"
+          element={
+            <JobInfo />
+          }
+        />
       </Routes>
     </Router>
   )
