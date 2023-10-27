f5d3916cd909ec32f911b9b48a02ae1696d318ee
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-27 17:39:39 -0400

    refactor: homePage Vacancy amp

--- src/features/shared/pages/home-page.jsx
+++ src/features/shared/pages/home-page.jsx
@@ -1,13 +1,36 @@
-import { VacancyCard } from "../../job-vacancy"
+import { useEffect, useState } from 'react'
+import { VacancyCard } from '../../job-vacancy'
+import { vacantApi } from '../../../api/vacant-api'
 
 export const HomePage = () => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await vacantApi.getVacancies()
+        setData(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-      <VacancyCard />
-      <VacancyCard />
-      <VacancyCard />
-      <VacancyCard />
-      <VacancyCard />
+      {data !== null ? (
+        data.map((vacancy) => (
+          <VacancyCard
+            key={vacancy.id}
+            title={vacancy.title}
+            description={vacancy.description}
+            salary={vacancy.salary}
+          />
+        ))
+      ) : (
+        <h2>No hay Vacantes Disponibles</h2>
+      )}
     </section>
   )
 }
