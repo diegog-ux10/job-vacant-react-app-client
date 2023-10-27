9f2c09e2ed4b878b8d5d497ef916cb72921cc869
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-27 17:33:52 -0400

    refactor: added props to VacancyCard

--- src/features/job-vacancy/components/vacancy-card.jsx
+++ src/features/job-vacancy/components/vacancy-card.jsx
@@ -1,20 +1,21 @@
-export const VacancyCard = () => {
+import PropTypes from 'prop-types';
+
+export const VacancyCard = ({title, description, salary}) => {
   return (
     <section className="bg-white rounded-lg shadow-md p-4">
       <header className="mb-2">
         <h2 className="text-2xl font-semibold text-blue-500">
-          React Developer Jr.
+          {title}
         </h2>
         <span className="text-gray-600">IBM</span>
         <p className="text-gray-600">Ciudad de México</p>
       </header>
       <main className="mb-4">
         <p className="text-gray-800">
-          Se busca Desarrollador Junior en React, con 6 meses de experiencia,
-          que pueda manejar Node.js, Redux, JavaScript y TypeScript
+          {description}
         </p>
         <h4 className="text-lg font-semibold text-blue-500 mt-2">
-          Salario: $1,000 - $1,500
+         {salary}
         </h4>
       </main>
       <footer className="flex justify-between">
@@ -31,3 +32,9 @@ export const VacancyCard = () => {
     </section>
   )
 }
+
+VacancyCard.propTypes = {
+  description: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  salary: PropTypes.string.isRequired,
+};
