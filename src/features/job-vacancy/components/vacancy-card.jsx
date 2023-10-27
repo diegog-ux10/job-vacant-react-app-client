74d71fd5e3269e6d8691afa9fe75f56e39a0a036
VRXtucci <tucci.jose023@gmail.com>
2023-10-27 09:45:55 -0400

    Actualizando estilos de las Cards de las Vacantes

--- src/features/job-vacancy/components/vacancy-card.jsx
+++ src/features/job-vacancy/components/vacancy-card.jsx
@@ -1,14 +1,33 @@
 export const VacancyCard = () => {
   return (
-    <div className='flex shadow-md bg-white'>
-      <div className='flex flex-col m-4'>
-        <h2 className='text-xl text-blue-500'>React Developer Jr.</h2>
-        <span className='font-bold'>IBM</span>
-        <label className='text-gray-400'>Ciudad de México</label>
-        <p className='my-4 font-semibold'>Se busca Desarrollador Junior en React, con 6 meses de experiencia, que pueda manejar Node.js, Redux, JavaScript y TypeScript</p>
-        <span className='text-gray-400 mb-4'>Salario: $1,000 - $1,500</span>
-        <a className=' bg-blue-500 rounded-md text-white text-center hover:bg-blue-600 w-1/3'>Aplicar ahora</a>
-      </div>
-    </div>
+    <section className="bg-white rounded-lg shadow-md p-4">
+      <header className="mb-2">
+        <h2 className="text-2xl font-semibold text-blue-500">
+          React Developer Jr.
+        </h2>
+        <span className="text-gray-600">IBM</span>
+        <p className="text-gray-600">Ciudad de México</p>
+      </header>
+      <main className="mb-4">
+        <p className="text-gray-800">
+          Se busca Desarrollador Junior en React, con 6 meses de experiencia,
+          que pueda manejar Node.js, Redux, JavaScript y TypeScript
+        </p>
+        <h4 className="text-lg font-semibold text-blue-500 mt-2">
+          Salario: $1,000 - $1,500
+        </h4>
+      </main>
+      <footer className="flex justify-between">
+        <a href="#" className="text-blue-500 hover:underline">
+          Más detalles
+        </a>
+        <a
+          href="#"
+          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+        >
+          Aplicar ahora
+        </a>
+      </footer>
+    </section>
   )
 }
