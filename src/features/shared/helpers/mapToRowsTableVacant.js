f0b1901353e9d7521f0eaa599ab7e446bf564366
VRXtucci <tucci.jose023@gmail.com>
2023-10-28 14:46:10 -0400

    mejorando codigo y agregandole una mensaje de consola

--- src/features/shared/helpers/mapToRowsTableVacant.js
+++ src/features/shared/helpers/mapToRowsTableVacant.js
@@ -1,12 +1,19 @@
 export const mapToRowsTableVacant = (responseArr) => {
-    const mappedArr = responseArr.map((vacancy)=>{
-        return {
-            id: vacancy.id,
-            title: vacancy.title,
-            description: vacancy.description,
-            salary: vacancy.salary
-        }
-    })
-
-    return mappedArr
-} 
+    if (!Array.isArray(responseArr)) {
+      // Manejar el caso en el que responseArr no sea un array
+      console.error('responseArr no es un array válido:', responseArr);
+      return [];
+    }
+  
+    const mappedArr = responseArr.map((vacancy) => {
+      return {
+        id: vacancy.id,
+        title: vacancy.title,
+        description: vacancy.description,
+        salary: vacancy.salary
+      };
+    });
+  
+    return mappedArr;
+  };
+  
