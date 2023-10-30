d278a68f44900b7553eaa18c29141eb5d8434979
VRXtucci <tucci.jose023@gmail.com>
2023-10-30 03:53:53 -0400

    Dandole estilos mejores a las alertas de mes

--- src/features/shared/components/feedback-mssg.jsx
+++ src/features/shared/components/feedback-mssg.jsx
@@ -1,28 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { removeMssg } from '../../../redux/slices/site-slice'
+import { useDispatch, useSelector } from 'react-redux';
+import { removeMssg } from '../../../redux/slices/site-slice';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ErrorIcon from '@mui/icons-material/Error';
 
 export const FeedbackMssg = () => {
-  const { mssg, mssgStatus } = useSelector((state) => state.site)
-  const dispatch = useDispatch()
+  const { mssg, mssgStatus } = useSelector((state) => state.site);
+  const dispatch = useDispatch();
 
   const handleClick = () => {
-    dispatch(removeMssg())
+    dispatch(removeMssg());
   }
 
   return (
-    <div
-      className={`flex flex-col fixed z-50 top-0 left-0 w-full min-h-screen bg-black bg-opacity-75 justify-center items-center`}
-    >
-      <div className="flex flex-col w-1/3 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-center">
-        {
-          mssgStatus === 'success' && <CheckCircleIcon />
-        }
-                {
-          mssgStatus === 'error' && <ErrorIcon />
-        }
-        <p className='bold'>{mssg}</p>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
+      <div className="w-1/3 bg-white shadow-md rounded p-8 text-center">
+        {mssgStatus === 'success' && <CheckCircleIcon className="text-green-500 text-6xl mx-auto" />}
+        {mssgStatus === 'error' && <ErrorIcon className="text-red-500 text-6xl mx-auto" />}
+        <p className="font-bold text-xl mb-4">{mssg}</p>
         <button
           onClick={handleClick}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -31,5 +25,5 @@ export const FeedbackMssg = () => {
         </button>
       </div>
     </div>
-  )
+  );
 }
